c4927630781bd7a972e3d3f2818f904e1e51a521
milk_wang <1248160312@qq.com>
2022-06-15 14:13:05 +0800

    fix: 123

--- src/1.js
+++ src/1.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>12131231213</h1>
+  return <h1>12131231231213</h1>
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
